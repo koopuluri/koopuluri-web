622dec9ecbc8ca44f5f4795d79948fff5988d668
koopuluri <koopuluri@gmail.com>
2016-02-20 21:05:37 -0500

    meet-at-tech post

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ var app      = express();
 var port     = process.env.PORT || 8080;
 var http = require('http');
 
+var Mixpanel = require('mixpanel').init("04f0999a4b69c79092856cd192fd5a14")
+
 var POSTS = require('./post-index');
 var PICTURES = require('./picture-index');
+
+var DEBUG = true;
+
+var log = function(key) {
+	if (DEBUG) {
+		// don't log
+	} else {
+		Mixpanel.track(key)
+	}
+}
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
@@ -16,9 +28,10 @@ app.use("/", express.static(__dirname + "/static"));
 
 // some routing:
 app.get('/', function(req, res, next) {
-   res.render('index.ejs', {
-   		posts: POSTS
-   });
+	Mixpanel.track("landing")
+	res.render('index.ejs', {
+		posts: POSTS
+	});
 });
 
 // =============== MAIN PAGE LINKS ================
@@ -30,6 +43,7 @@ app.get('/posts', function(req, res, next) {
 });
 
 app.get('/photography', function(req, res, next) {
+	Mixpanel.track("photography")
 	res.render('photography.ejs', {
 		pictures: PICTURES
 	});
@@ -37,6 +51,11 @@ app.get('/photography', function(req, res, next) {
 
 app.get('/resume', function(req, res, next) {
 	res.render('resume.ejs', {});
+});
+
+app.get('/meet', function(req, res, next) {
+	Mixpanel.track("meet")
+	res.render('../posts/meets.ejs', {})
 });
 
 // ================================================
@@ -57,10 +76,17 @@ app.get('/vernon', function(req, res, next) {
 var server = http.createServer(app).listen(port, function() {
     console.log('Express server listening on port ' + port);
 
+    if (port == 8080) {
+    	DEBUG = true
+    } else {
+    	DEBUG = false
+    }
+
 	for (var url in POSTS) {
 		app.get(url, function(req, res, next) {
 			var url = req.route.path;
 			var postFilepath = '../posts/' + url.slice(1) + '.ejs';
+			Mixpanel.track(POSTS[url]['title'])
 			res.render(postFilepath, {
 				title: POSTS[url]['title'],
 				timestamp: POSTS[url]['timestamp']
